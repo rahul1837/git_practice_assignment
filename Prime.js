0883f6f98017f9fb90c566a2731c08a1b76698a6
“Rahul <rahulkumar4303411@gmail.com>
2022-09-13 23:12:14 +0530

    message correction

---
+++ Prime.js
@@ -0,0 +1,15 @@
+
+
+ let num = 15;
+ let count =0;
+ for(let i=0;i<=num;i++)
+ {
+  if(num%i==0){
+  count++;
+  }
+ }
+  if(count ==3)
+  {
+  console.log("Prime no.");
+  }else{
+console.log("Not a Prime no.");
