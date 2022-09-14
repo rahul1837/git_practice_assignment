13baafeb9e563bb5468a2688cedd795e43376f68
“Rahul <rahulkumar4303411@gmail.com>
2022-09-14 21:35:53 +0530

    palindrome problem done

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+
+let str = "naman"
+let bag ="";
+for(let i=str.length;i>=0;i++)
+{
+  bag = bag+str[i];
+  }
+   if(bag==str)
+   {
+    console.log("Yes it's a palindrome");
+    }else{
+    console.log("Not a palindrome");
+ 
